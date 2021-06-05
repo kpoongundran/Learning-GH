102f23c1d2f9bb5737e53237ade759504b56ac20
Poongundran Krishnamurthi <kpoongundran@gmail.com>
2021-06-05 15:25:41 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Poongundran Krishnamurthi on 05/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
